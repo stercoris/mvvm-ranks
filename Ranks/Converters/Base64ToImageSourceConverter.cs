d9027bbf29b1bd61f1abed49d493a1a2ee93b8b4
Leeeeeeeeeeeeeeeeeeeee <dimarodin0@gmail.com>
2021-03-25 20:00:17 +0400

    Base64String to ImageSource Converter

---
+++ Ranks/Converters/Base64ToImageSourceConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Ranks.Converters
+{
+    class Base64ToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+
+            int width = (int)parameter;
+            int height = width/9*16;
+            string base64String = (string)value ;
+
+            bitmapImage.BeginInit();
+            bitmapImage.DecodePixelWidth = width;
+            bitmapImage.DecodePixelHeight = height;
+            bitmapImage.CacheOption = BitmapCacheOption.None;
+            if (base64String == null)
+            {
+                return bitmapImage;
+            }
+            bitmapImage.StreamSource = new MemoryStream(System.Convert.FromBase64String(base64String));
+            bitmapImage.EndInit();
+
+            return (bitmapImage);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            BitmapSource image = (BitmapSource)value;
+            MemoryStream outStream = new MemoryStream();
+            BitmapEncoder enc = new BmpBitmapEncoder();
+            enc.Frames.Add(BitmapFrame.Create(image));
+            enc.Save(outStream);
+            Bitmap bitmap = new Bitmap(outStream);
+            Bitmap bm = new Bitmap(bitmap);
+            MemoryStream ms = new MemoryStream();
+            bm.Save(ms, ImageFormat.Jpeg);
+            byte[] byteImage = ms.ToArray();
+            outStream.Close();
+            ms.Close();
+            return (System.Convert.ToBase64String(byteImage));
+        }
+    }
+}
