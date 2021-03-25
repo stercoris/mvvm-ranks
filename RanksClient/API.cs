6ce2ea334d707cfa6678512d6d82be38b3d74211
Leeeeeeeeeeeeeeeeeeeee <dimarodin0@gmail.com>
2021-03-25 23:51:25 +0400

    Codegen realisation, deleted old api

--- RanksClient/API.cs
+++ RanksClient/API.cs
@@ -9,23 +9,16 @@ using System.Windows.Media.Imaging;
 using GraphQL.Client.Http;
 using GraphQL.Client.Serializer.Newtonsoft;
 using Newtonsoft.Json;
-using RanksClient.Resolvers;
 
 namespace RanksClient
 {
     public class API
     {
-        private GraphQLHttpClient graphQLClient;
-        public GroupResolver GroupResolver { get; private set; }
-        public UserResolver UserResolver { get; private set; }
-        public RankResolver RankResolver { get; private set; }
+        public GraphQLHttpClient graphQLClient;
 
         public API(string Url)
         {
             this.graphQLClient = new GraphQLHttpClient(Url, new NewtonsoftJsonSerializer(new JsonSerializerSettings()));
-            GroupResolver = new GroupResolver(graphQLClient);
-            UserResolver = new UserResolver(graphQLClient);
-            RankResolver = new RankResolver(graphQLClient);
         }
 
     }
