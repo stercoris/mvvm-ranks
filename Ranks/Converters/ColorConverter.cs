5249453e14af40ce5609a87be8484ed5d551b93b
a000011 <anton.nadikin@gmail.com>
2021-03-27 23:18:55 +0300

    Added top 3

--- Ranks/Converters/ColorConverter.cs
+++ Ranks/Converters/ColorConverter.cs
@@ -11,20 +11,12 @@ using Ranks.ViewModels;
 namespace Ranks.Converters
 {
 
-    public class TopUsersConverter : IValueConverter
+    public class ColorConverter : IValueConverter
     {
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            
-            var UsersList = (value as ObservableCollection<UserViewModel>).ToList();
-            UsersList = UsersList.OrderBy(user => System.Convert.ToInt32(user.User.rankId)).ToList();
-            var topUsers = new ObservableCollection<UserViewModel>();
-            foreach (var user in UsersList.GetRange(0   , (UsersList.Count > 3) ? 3: UsersList.Count))
-            {
-                topUsers.Add(user);
-            }
-            return topUsers;
+            return 1;
         }
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
