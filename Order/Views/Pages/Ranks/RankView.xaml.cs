22ddbf3080b7ec769d241403efb3e1cce2a5a351
антон надькин <anton.nadikin@gmail.com>
2021-04-02 23:03:46 +0300

    Styles refactoring, some design changes

--- Order/Views/Pages/Ranks/RankView.xaml.cs
+++ Order/Views/Pages/Ranks/RankView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 
 namespace Order.View
 {
@@ -10,6 +13,25 @@ namespace Order.View
         public RankView()
         {
             InitializeComponent();
+        }
+
+        private void ExpandRankClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+            double startPoint = ActualHeight;
+            double finishPoint = 150;
+            RotateTransform rotateTransform = new(180);
+            if (startPoint == finishPoint)
+            {
+                finishPoint = 60;
+                rotateTransform = new(0);
+            }
+            DoubleAnimation rankSizeAnimation = new();
+            rankSizeAnimation.From = startPoint;
+            rankSizeAnimation.To = finishPoint;
+            rankSizeAnimation.Duration = TimeSpan.FromSeconds(0.3);
+            BeginAnimation(Button.HeightProperty, rankSizeAnimation);
+
+            (sender as Button).RenderTransform = rotateTransform;
         }
     }
 }
